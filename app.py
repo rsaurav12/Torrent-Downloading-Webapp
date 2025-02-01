6896e51f9aa44d3cce2b281ea9da61942bb3a4eb
nishantprime <nishantkumarprime@gmail.com>
2025-02-02 02:07:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -148,7 +148,7 @@ def download_files(session_id, selected_indices):
         save_session(session_id, session)
         # Update progress in a loop
         
-        while handle.status().state != 'seeding' and handle.status().state != 'finished':
+        while handle.status().state != 'seeding' or handle.status().state != 'finished':
             status = handle.status()
             session = load_session(session_id) or {}
             session.update({
